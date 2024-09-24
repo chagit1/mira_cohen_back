35cfe2f6b4aa1bcd7c5ef06dd4abd0036fb673f0
dini_landy <dini0527197044@gmail.com>
2024-09-25 00:25:46 +0300

    remove

--- Repository/Model/Institution.cs
+++ Repository/Model/Institution.cs
@@ -1,63 +1,4 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.ComponentModel.DataAnnotations.Schema;
-//using System.ComponentModel.DataAnnotations;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
-//using MongoDB.Bson.Serialization.Attributes;
-//using MongoDB.Bson;
-
-//namespace Repository
-//{
-//    public class Institution
-//    {
-//        [BsonId]
-//        [BsonRepresentation(BsonType.ObjectId)]
-//        public string Id { get; set; }
-
-//        [BsonElement("User")]
-//        public User User { get; set; }
-
-//        [BsonElement("InstitutionName")]
-//        [BsonIgnoreIfNull]
-//        public string InstitutionName { get; set; }
-
-//        [BsonElement("Symbol")]
-//        [BsonIgnoreIfNull]
-//        public string Symbol { get; set; }
-
-//        [BsonElement("ManagerName")]
-//        [BsonIgnoreIfNull]
-//        public string ManagerName { get; set; }
-
-//        [BsonElement("ContactPerson")]
-//        [BsonIgnoreIfNull]
-//        public string ContactPerson { get; set; }
-
-//        [BsonElement("ContactPhone")]
-//        [BsonIgnoreIfNull]
-//        public string ContactPhone { get; set; }
-
-//        [BsonElement("ContactEmail")]
-//        [BsonIgnoreIfNull]
-//        public string ContactEmail { get; set; }
-
-//        [BsonElement("InspectorName")]
-//        [BsonIgnoreIfNull]
-//        public string InspectorName { get; set; }
-
-//        [BsonElement("Students")]
-//        [BsonIgnoreIfNull]
-//        public List<Student>? Students { get; set; }
-//        public Institution(string inspectorName)
-//        {
-//            this.InspectorName = inspectorName;
-//            Students = new List<Student>();
-//        }
-//    }
-//}
-using MongoDB.Bson.Serialization.Attributes;
+﻿using MongoDB.Bson.Serialization.Attributes;
 using MongoDB.Bson;
 using Repository;
 
@@ -106,5 +47,4 @@ public class Institution
             this.InspectorName = inspectorName;
             Students = new List<Student>();
         }
-    }
 }
